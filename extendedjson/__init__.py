41a93490ce3b62a5bfe50b1b8c4c5991ef0c8d23
Rodrigo Girão Serrão <5621605+RodrigoGiraoSerrao@users.noreply.github.com>
2022-06-21 17:19:10 +0100

    Add ExtendedEncoder and ExtendedDecoder.

--- extendedjson/__init__.py
+++ extendedjson/__init__.py
@@ -1,1 +1,31 @@
 __version__ = "0.1.0"
+
+import json
+
+
+class ExtendedEncoder(json.JSONEncoder):
+    def default(self, obj):
+        name = type(obj).__name__
+        try:
+            encoder = getattr(self, f"encode_{name}")
+        except AttributeError:
+            super().default(obj)
+        else:
+            encoded = encoder(obj)
+            encoded["__extended_json_type__"] = name
+            return encoded
+
+
+class ExtendedDecoder(json.JSONDecoder):
+    def __init__(self, **kwargs):
+        kwargs["object_hook"] = self.object_hook
+        super().__init__(**kwargs)
+
+    def object_hook(self, obj):
+        try:
+            name = obj["__extended_json_type__"]
+            decoder = getattr(self, f"decode_{name}")
+        except (KeyError, AttributeError):
+            return obj
+        else:
+            return decoder(obj)
